dd6796583db0eb15a2e3d57885156242601d7e62
masterliu <helbreatha@hotmail.com>
2016-10-10 14:14:04 +0800

    下载网易音乐中文件

---
+++ lession.py
@@ -0,0 +1,14 @@
+'''
+下载网易 音乐
+'''
+import requests
+import  urllib
+r = requests.get('http://music.163.com/api/playlist/detail?id=3779629')
+
+arr = r.json()['result']['tracks']
+
+for i in range(10):
+    name = str(i+1)+' '+arr[i]['name']+'.mp3'
+    link = arr[i]['mp3Url']
+    urllib.urlretrieve(link,'d:\pythonbin/'+name)
+    print(name+'  down')
