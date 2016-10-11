bfb2789e0593c5f211ab999de491f114b4e53d54
masterliu <helbreatha@hotmail.com>
2016-10-11 17:34:58 +0800

    part 100

---
+++ part 100/part8.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+import time
+for i in range(1,10):
+    for j in range(1,10):
+        rest=i * j
+        print ('%d * %d=% -3d'%(i,j,rest))
+    print ' '
+
+print time.ctime()
+time.sleep(1)
+print time.ctime()
