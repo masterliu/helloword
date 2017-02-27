abf54ae0b86c75b6ea696681dc80c41412bc2bd5
MrLiu <helbreatha@hotmail.com>
2017-02-27 13:11:11 +0800

    just test

--- part 100/part20.py
+++ part 100/part20.py
@@ -4,11 +4,11 @@ Sn = 100.0
 Hn = Sn / 2
 
 Sn += 2 * 2
-print Sn
+print(Sn)
 
 for n in range(2, 11):
     Sn += 2 * Hn
     Hn /= 2
 
-print 'total of road is %f' % Sn
-print 'the tenth is %f meter' % Hn
+print('total of road is %f' % Sn)
+print('the tenth is %f meter' % Hn)
