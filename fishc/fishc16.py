c3074f620561a5bdf4c11d2a63812a61b1791715
masterliu <helbreatha@hotmail.com>
2017-04-07 18:01:04 +0800

    just test

--- fishc/fishc16.py
+++ fishc/fishc16.py
@@ -2,66 +2,69 @@
 
 import urllib.request
 import os
-
+import shutil
 
 def url_open(url):
-    req = urllib.request.Request(url)
-    req.add_header('User-Agent',
-                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36')
-    response = urllib.request.urlopen(url)
-    html = response.read()
+	req = urllib.request.Request(url)
+	req.add_header('User-Agent',
+				   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+				   'Chrome/57.0.2987.110 Safari/537.36')
+	response = urllib.request.urlopen(url)
+	html = response.read()
 
-    print(url)
-    return html
+	# print(url)
+	return html
 
 
 def get_page(url):
-    html = url_open(url).decode('utf-8')
-    a = html.find('current-comment-page') + 23
-    b = html.find(']', a)
-    return html[a:b]
+	html = url_open(url).decode('utf-8')
+	# print ('get_page',html)
+	a = html.find('current-comment-page') + 23
+	b = html.find(']', a)
+	return html[a:b]
 
 
 def find_imgs(url):
-    html = url_open(url).decode('utf-8')
-    img_adds = []
-    a = html.find('img src=')
-    while a != -1:
-        b = html.find('.jpg', a, a + 255)
-        if b != -1:
-            img_adds.append(html[a + 9:b + 4])
-        else:
-            b = a + 9
-        a = html.find('src=', b)
+	html = url_open(url).decode('utf-8')
+	img_adds = []
+	a = html.find('img src=')
+	while a != -1:
+		b = html.find('.jpg', a, a + 255)
+		if b != -1:
+			img_adds.append(html[a + 9:b + 4])
+		else:
+			b = a + 9
+		a = html.find('src=', b)
 
-    for each in img_adds:
-        print(each)
+	for each in img_adds:
+		print(each)
 
-    return img_adds
+	return img_adds
 
 
 def save_imgs(folder, img_adds):
-    for each in img_adds:
-        filename = each.split('/')[-1]
-        with open(filename, 'wb') as f:
-            img = url_open(each)
-            print (each)
-            f.write(img)
+	for each in img_adds:
+		filename = each.split('/')[-1]
+		with open(filename, 'wb') as f:
+			img = url_open(each)
+			# print(img)
+			f.write(img)
 
 
 def download_mm(folder='OOXX', pages=5):
-    os.mkdir(folder)
-    os.chdir(folder)
+	shutil.rmtree(folder)
+	os.mkdir(folder)
+	os.chdir(folder)
 
-    url = "http://jandan.net/ooxx/"
-    page_num = int(get_page(url))
-
-    for i in range(pages):
-        page_num -= i
-        page_url = url + 'page-' + str(page_num) + '#comments'
-        img_adds = find_imgs(page_url)
-        save_imgs(folder, img_adds)
+	url = "http://jandan.net/ooxx/"
+	page_num = int(get_page(url))
+	# print (page_num)
+	for i in range(pages):
+		page_num -= i
+		page_url = url + 'page-' + str(page_num) + '#comments'
+		img_adds = find_imgs(page_url)
+		save_imgs(folder, img_adds)
 
 
 if __name__ == '__main__':
-    download_mm()
+	download_mm()
