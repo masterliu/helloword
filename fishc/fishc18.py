75e0570b3082c29e25b9f7374ed4c761c8a255d1
MrLiu <helbreatha@hotmail.com>
2017-04-11 22:50:02 +0800

    update fishc18 正则表达式 匹配IP地址

--- fishc/fishc18.py
+++ fishc/fishc18.py
@@ -8,8 +8,8 @@ def url_open(url):
     req.add_header('User-Agent',
                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                    'Chrome/57.0.2987.110 Safari/537.36')
-    response = urllib.request.urlopen(req)
-    html = response.read().decode('utf-8')
+    page = urllib.request.urlopen(req)
+    html = page.read().decode('utf-8')
 
     return html
 
@@ -17,14 +17,14 @@ def url_open(url):
 #  ip地址的正则表达式
 
 def get_img(html):
-    p = r'((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))"'
+    # p = r'((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))"'
+    p = r'(?:(?:[0,1]?\d|2[0-4]\d|25[0-5])\.){3}(?:[0,1]?\d|2[0-4]\d|25[0-5])'
     iplist = re.findall(p, html)
 
-    print (iplist)
     for each in iplist:
         print(each)
 
 
 if __name__ == '__main__':
-    url = 'http://cn-proxy.com'
+    url = 'http://www.xicidaili.com/'
     get_img(url_open(url))
